750fc696a8b98f6cc8ae5ae6be9b91511c7e97fa
Galina Troshkina <maystar2009@gmail.com>
2022-12-23 00:41:20 +0300

    Styled forms author, book, note, login, signup

--- books/forms.py
+++ books/forms.py
@@ -7,9 +7,9 @@ from .models import Author, Book, Collection, Note
 
 
 class AuthorForm(forms.Form):
-    last_name = forms.CharField(label="Author's last name", max_length=100)
-    first_name = forms.CharField(label="Author's first name", max_length=100)
-    middle_name = forms.CharField(label="Author's middle name (optional)", max_length=100, required=False)
+    last_name = forms.CharField(label="Author's last name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label="Author's first name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    middle_name = forms.CharField(label="Author's middle name (optional)", max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 
 class BookForm(ModelForm):
@@ -34,12 +34,30 @@ class CollectionForm(ModelForm):
 
 
 class CreationForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email']
+        fields = ['email', 'username', 'first_name', 'last_name']
+
+    def __init__(self, *args, **kwargs):
+        super(CreationForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        
 
 
 class NoteForm(ModelForm):
     class Meta:
         model = Note
-        fields = ('book', 'heading', 'text')
+        fields = ['book', 'heading', 'text']
+
+        widgets = {
+            'book': forms.Select(attrs={'class': 'form-control'}),
+            'heading': forms.TextInput(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={'class': 'form-control'})
+        }
